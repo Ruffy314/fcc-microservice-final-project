1c5cc06587d457f54b8f73000cba02c431be1a6b
Ruffy314 <Ruffy314@users.noreply.github.com>
2021-04-19 02:54:16 +0200

    API implemented
    API works locally

--- index.js
+++ index.js
@@ -3,7 +3,28 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/static/index.html");
-})
+});
+
+app.get("/api/timestamp/:timestring", (req, res) => {
+  const str = req.params.timestring;
+  let date;
+  const fmtStr = "ddd, DD MMM YYYY HH:mm:ss Z";
+  if (str.match(/^\d+$/)) {
+    // unix timestamp
+    date = new Date(parseInt(str));
+    res.json({unix: date.valueOf(), utc: date.toUTCString()});
+
+  } else if (str.match(/^\d{4}-\d{2}-\d{2}/)) {
+    //ISO format
+    date = new Date(str);
+    res.json({unix: date.valueOf(), utc: date.toUTCString()});
+
+  } else {
+    // invalid format
+    res.json({error:"Invalid Date"});
+  }
+  
+});
 
 app.listen(3000);
 console.log("Server listens on port 3000");
