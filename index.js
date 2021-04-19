4511c6f6ea58676b7c832bec5da0a6410ab7477f
Ruffy314 <>
2021-04-19 02:00:40 +0000

    new route, handling missing params, logging

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
+  console.log(string = req.method + " " + req.path + " - " + req.ip + "("+req.originalUrl+")");
   res.sendFile(__dirname + "/static/index.html");
 });
 
+app.get("/api/timestamp", (req, res) => {
+  console.log(string = req.method + " " + req.path + " - " + req.ip + "("+req.originalUrl+")");
+  const date = new Date();
+  res.json({unix: date.valueOf(), utc: date.toUTCString()});
+})
+
 app.get("/api/timestamp/:timestring", (req, res) => {
+  console.log(string = req.method + " " + req.path + " - " + req.ip + "("+req.originalUrl+")");
   const str = req.params.timestring;
   let date;
-  const fmtStr = "ddd, DD MMM YYYY HH:mm:ss Z";
   if (str.match(/^\d+$/)) {
     // unix timestamp
     date = new Date(parseInt(str));
@@ -23,6 +30,33 @@ app.get("/api/timestamp/:timestring", (req, res) => {
     // invalid format
     res.json({error:"Invalid Date"});
   }
+  
+});
+
+app.get("/api", (req, res) => {
+  console.log(string = req.method + " " + req.path + " - " + req.ip + "("+req.originalUrl+")");
+  const date = new Date();
+  res.json({unix: date.valueOf(), utc: date.toUTCString()});
+})
+
+app.get("/api/:timestring", (req, res) => {
+  console.log(string = req.method + " " + req.path + " - " + req.ip + "("+req.originalUrl+")");
+  const str = req.params.timestring;
+  let date;
+  if (str.match(/^\d{5,}$/)) {
+    // unix timestamp
+    date = new Date(parseInt(str));
+    res.json({unix: date.valueOf(), utc: date.toUTCString()});
+
+  } else {
+    //ISO format
+    date = new Date(str);
+    if (date === "Invalid Date") {
+      res.json({error:"Invalid Date"});
+    } else {
+      res.json({unix: date.valueOf(), utc: date.toUTCString()});
+    }
+  } 
   
 });
 
